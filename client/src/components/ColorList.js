53229b050b8a6577024a8bc780821bd04d3921c4
Kenna <kennaalastair@gmail.com>
2019-09-21 11:44:21 -0700

    delete request error

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, history }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
@@ -25,7 +26,16 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/api/colors/${color.id}`)
+      .then(res => {
+        updateColors(res.data);
+        history.push('/protected');
+      })
+      .catch(err => console.log(err.response));
   };
+
+  //console.log(colors);
 
   return (
     <div className="colors-wrap">
